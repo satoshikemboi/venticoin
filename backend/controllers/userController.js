4d1fe6725d9ddf925629dd67f484f8a284934997
satoshikemboi <satoshikemboi@gmail.com>
2026-01-24 18:20:04 +0300

    modified the user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,1 +1,23 @@
 
+// backend/routes/userRoutes.js
+import express from "express";
+import { 
+  getUsers, 
+  getUserById, // Make sure this is included
+  updateUser, 
+  deleteUser, 
+  signupUser, 
+  loginUser 
+} from "../controllers/userController.js";
+
+const router = express.Router();
+
+// Update your routes to use the correct names
+router.post("/signup", signupUser); // Changed from createUser to signupUser
+router.post("/login", loginUser);
+router.get("/", getUsers);
+router.get("/:id", getUserById);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+
+export default router;
